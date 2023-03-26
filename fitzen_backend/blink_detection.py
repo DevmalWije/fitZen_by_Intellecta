3eb6be2c0c6842bc2b31ec4b7fd663fa1168e429
Devmal wijesingha <devmalwije1@gmail.com>
2023-03-26 20:05:11 +0530

    ml models can now be called externally + pose log

--- fitzen_backend/blink_detection.py
+++ fitzen_backend/blink_detection.py
@@ -9,48 +9,94 @@ counter = 0
 start_time = datetime.datetime.now()
 
 face_detector = dlib.get_frontal_face_detector()
-eye_landmark_predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+eye_landmark_predictor = dlib.shape_predictor(
+    "shape_predictor_68_face_landmarks.dat")
 
-#to get the middle point of an eye to draw vertical line
+# main export function
+
+
+def detect_blinks(frame):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_detector(gray)
+    blink_count = 0
+
+    for face in faces:
+        eye_landmarks = eye_landmark_predictor(gray, face)
+        left_eye_ratio = get_ratio_of_blinking(
+            [36, 37, 38, 39, 40, 41], eye_landmarks)
+        right_eye_ratio = get_ratio_of_blinking(
+            [42, 43, 44, 45, 46, 47], eye_landmarks)
+
+        # to get the average ration of both eye
+        EAR = (left_eye_ratio + right_eye_ratio)/2
+
+        if EAR > 5.7:
+            blink_count += 1
+            print(blink_count, "blink detected")
+
+    cv2.imshow("Frame", frame)
+
+    if (time_interval_passed()):
+        if (counter < 10):
+            print("Alert!")
+        counter = 0
+
+    key = cv2.waitKey(1)
+
+    return blink_count, key
+
+# to get the middle point of an eye to draw vertical line
+
+
 def mid_point(p1, p2):
     return int((p1.x + p2.x)/2), int((p1.y + p2.y)/2)
 
+
 font = cv2.FONT_HERSHEY_PLAIN
 
+
 def get_ratio_of_blinking(points_of_eye, facila_landmarks):
-    #to get the left and right point of an eye to draw a horizontal line
-        left_point = (facila_landmarks.part(points_of_eye[0]).x, facila_landmarks.part(points_of_eye[0]).y)
-        right_point = (facila_landmarks.part(points_of_eye[3]).x, facila_landmarks.part(points_of_eye[3]).y)
+    # to get the left and right point of an eye to draw a horizontal line
+    left_point = (facila_landmarks.part(
+        points_of_eye[0]).x, facila_landmarks.part(points_of_eye[0]).y)
+    right_point = (facila_landmarks.part(
+        points_of_eye[3]).x, facila_landmarks.part(points_of_eye[3]).y)
 
-        #to get the top and bottom 2 points to find the mid point of them
-        top_center = mid_point(facila_landmarks.part(points_of_eye[1]), facila_landmarks.part(points_of_eye[2]))
-        bottom_center = mid_point(facila_landmarks.part(points_of_eye[5]), facila_landmarks.part(points_of_eye[4]))
+    # to get the top and bottom 2 points to find the mid point of them
+    top_center = mid_point(facila_landmarks.part(
+        points_of_eye[1]), facila_landmarks.part(points_of_eye[2]))
+    bottom_center = mid_point(facila_landmarks.part(
+        points_of_eye[5]), facila_landmarks.part(points_of_eye[4]))
 
-        #to draw the horizontal line in an eye
-        horizontal_line = cv2.line(frame, left_point, right_point, (0, 255, 0), 2)
+    # to draw the horizontal line in an eye
+    horizontal_line = cv2.line(frame, left_point, right_point, (0, 255, 0), 2)
 
-        #to vertical line in eye
-        vertical_line = cv2.line(frame, top_center, bottom_center, (0, 255, 0), 2)
+    # to vertical line in eye
+    vertical_line = cv2.line(frame, top_center, bottom_center, (0, 255, 0), 2)
 
-        length_of_ver_line = hypot((top_center[0] - bottom_center[0]), (top_center[1] - bottom_center[1]))
-        
-        length_of_hor_line = hypot((left_point[0] - right_point[0]), (left_point[1] - right_point[1]))
+    length_of_ver_line = hypot(
+        (top_center[0] - bottom_center[0]), (top_center[1] - bottom_center[1]))
 
-        EAR = length_of_hor_line/length_of_ver_line
-        return EAR
+    length_of_hor_line = hypot(
+        (left_point[0] - right_point[0]), (left_point[1] - right_point[1]))
+
+    EAR = length_of_hor_line/length_of_ver_line
+    return EAR
+
 
 def time_interval_passed():
 
-	global start_time
+    global start_time
 
-	# Calculate time interval since last iteration
-	time_delta = datetime.datetime.now() - start_time
-	seconds_passed = time_delta.total_seconds()
+    # Calculate time interval since last iteration
+    time_delta = datetime.datetime.now() - start_time
+    seconds_passed = time_delta.total_seconds()
 
-	# Check if one minute interval has passed
-	if(seconds_passed >= 60):
-		start_time = datetime.datetime.now()
-		return True
+    # Check if one minute interval has passed
+    if (seconds_passed >= 60):
+        start_time = datetime.datetime.now()
+        return True
+
 
 while True:
     _, frame = cap.read()
@@ -59,33 +105,33 @@ while True:
     faces = face_detector(gray)
 
     for face in faces:
-        #x, y = face.left(), face.top()
-        #x1, y1 = face.right(), face.bottom()
-        #cv2.rectangle(frame, (x,y), (x1,y1), (0, 255, 0), 2)
+        # x, y = face.left(), face.top()
+        # x1, y1 = face.right(), face.bottom()
+        # cv2.rectangle(frame, (x,y), (x1,y1), (0, 255, 0), 2)
 
         eye_landmarks = eye_landmark_predictor(gray, face)
 
-        left_eye_ratio = get_ratio_of_blinking([36, 37, 38, 39, 40, 41], eye_landmarks)
-        right_eye_ratio = get_ratio_of_blinking([42, 43, 44, 45, 46, 47], eye_landmarks)
+        left_eye_ratio = get_ratio_of_blinking(
+            [36, 37, 38, 39, 40, 41], eye_landmarks)
+        right_eye_ratio = get_ratio_of_blinking(
+            [42, 43, 44, 45, 46, 47], eye_landmarks)
 
-        #to get the average ration of both eye
+        # to get the average ration of both eye
         EAR = (left_eye_ratio + right_eye_ratio)/2
 
-        
         if EAR > 5.7:
-            #cv2.putText(frame, "BLINKING", (50, 150), font, 7, (255, 255, 255))
-            #cv2.putText(frame, f'Blink Count: {counter}', (50, 100), font, 7, (255, 0, 255))
+            # cv2.putText(frame, "BLINKING", (50, 150), font, 7, (255, 255, 255))
+            # cv2.putText(frame, f'Blink Count: {counter}', (50, 100), font, 7, (255, 0, 255))
             counter = counter+1
-            print(counter,"blink detect")
-             
+            print(counter, "blink detect")
+
     cv2.imshow("Frame", frame)
 
-    if(time_interval_passed()):
-        if(counter < 10):
+    if (time_interval_passed()):
+        if (counter < 10):
             print("Alert!")
         counter = 0
-        
-                        
+
     key = cv2.waitKey(1)
     if key == 25:
         break
--- fitzen_backend/optimisedModelImp.py
+++ fitzen_backend/optimisedModelImp.py
@@ -12,120 +12,87 @@ import warnings
 mp_drawing = mp.solutions.drawing_utils
 # pose detectionL
 mp_holistic = mp.solutions.holistic
-cap = cv2.VideoCapture(0)
 
-
-#loading trained model
+# loading trained model
 with open("randomForest_pose_classifierV1.pkl", "rb") as f:
-        model = pickle.load(f)
-        
-#initiating holistic model        
+    model = pickle.load(f)
+
+# initiating holistic model        
 holistic = mp.solutions.holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5)
+
+# create video capture object
+cap = cv2.VideoCapture(0)
     
-def image_frame_model(image_folder):
-    # image_names = sorted(os.listdir(image_folder))
+def image_frame_model(frame):
+    #getting current timestamp from local system
+    currentTime = datetime.datetime.now()
+    
     posture_list = []
+    posture_dict = {}
 
-    # for img_name in image_names:
-    #     img_path = os.path.join(image_folder, img_name)
-    #     frame = cv2.imread(img_path)
-    
-    while cap.isOpened():
-        
+    # Recolor Feed
+    image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    image.flags.writeable = False
 
-        ret, frame = cap.read()
-        
-        # Recolor Feed
-        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        image.flags.writeable = False
+    # Make Detections
+    results = holistic.process(image)
+    # print(results.face_landmarks)
+
+    # Recolor image back to BGR for rendering
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+    # Export coordinates
+    try:
+        # Extract Pose landmarks
+        pose = results.pose_landmarks.landmark
+        pose_row = list(np.array(
+            [[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in pose]).flatten())
+
+        # Extract Face landmarks
+        face = results.face_landmarks.landmark
+        face_row = list(np.array(
+            [[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in face]).flatten())
+
+        # Concate rows
+        row = pose_row+face_row
 
         # Make Detections
-        results = holistic.process(image)
-        # print(results.face_landmarks)
+        X = pd.DataFrame([row])
+        pose_language_class = model.predict(X)[0]
+        pose_language_prob = model.predict_proba(X)[0]
 
-        # Recolor image back to BGR for rendering
-        image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        # filtering out warning from stander scaler due to shaping issues
+        warnings.filterwarnings('ignore')
+        X = [[0, 1], [1, 0]]
+        scaler = StandardScaler()
+        scaler.fit(X)
+        posture_list.append(pose_language_class)
+        
+        #writing posture class and detected time to text file
+        with open("posture.txt", "a") as f:
+            f.write(f"{pose_language_class},{currentTime}\n")
 
-        # Export coordinates
-        try:
-            # Extract Pose landmarks
-            pose = results.pose_landmarks.landmark
-            pose_row = list(np.array(
-                [[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in pose]).flatten())
+        # if pose_language_class == "proper_posture":
+        #     print("Proper posture")
 
-            # Extract Face landmarks
-            face = results.face_landmarks.landmark
-            face_row = list(np.array(
-                [[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in face]).flatten())
-
-            # Concate rows
-            row = pose_row+face_row
-
-            # Make Detections
-            X = pd.DataFrame([row])
-            pose_language_class = model.predict(X)[0]
-            pose_language_prob = model.predict_proba(X)[0]
-
-            # filtering out warning from stander scaler due to shaping issues
-            warnings.filterwarnings('ignore')
-            X = [[0, 1], [1, 0]]
-            scaler = StandardScaler()
-            scaler.fit(X)
-            
-            # Grab ear coords
-            coords = tuple(np.multiply(
-                np.array(
-                    (results.pose_landmarks.landmark[mp_holistic.PoseLandmark.LEFT_EAR].x,
-                        results.pose_landmarks.landmark[mp_holistic.PoseLandmark.LEFT_EAR].y)), [640, 480]).astype(int))
-
-            cv2.rectangle(image,
-                            (coords[0], coords[1]+5),
-                            (coords[0]+len(pose_language_class)
-                                * 20, coords[1]-30),
-                            (245, 117, 16), -1)
-            cv2.putText(image, pose_language_class, coords,
-                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-
-            # Get status box
-            cv2.rectangle(image, (0, 0), (340, 60), (245, 117, 16), -1)
-
-            # Display Class
-            cv2.putText(image, 'CLASS', (95, 12),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
-
-            cv2.putText(image, pose_language_class.split(' ')[
-                        0], (90, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-
-            # Display Probability
-            cv2.putText(image, 'PROB', (15, 12),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
-
-            cv2.putText(image, str(round(pose_language_prob[np.argmax(pose_language_prob)], 2)), (
-                10, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 2, cv2.LINE_AA)
-
-
-            posture_list.append(pose_language_class)
-
-            if pose_language_class == "proper_posture":
-                print("Proper posture")
-
-        except:
-            print("No posture detected")
-            pass
-
-        cv2.imshow('Raw Webcam Feed', image)
-
-        if cv2.waitKey(50) & 0xFF == ord('q'):
-            break
-
+    except:
+        print("No posture detected")
+        pass
+    
+    # counting the number of proper posture
     proper_posture_count = posture_list.count("proper_posture")
 
+    # calculating the percentage of proper posture
     proper_posture_percentage = round(
         (proper_posture_count/len(posture_list))*100, 1)
+    
+    # #
+    # print("Proper posture percentage: ", proper_posture_percentage)
+    
+    #adding the posture class and image frame to a dictionary
+    posture_dict = {'posture_class': pose_language_class, 'image_frame': image}
+    
+    return posture_dict
 
-    print("Proper posture percentage: ", proper_posture_percentage)
 
-if __name__ == '__main__':
-    image_folder = "D:/IIT courswork/L-5-SE-2023/5COSC021C.2 Software Development Group Project/SDGP group-work/fitZen_by_Intellecta/frames"
-    image_frame_model(image_folder)
