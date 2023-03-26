fd0353333248eff2329883536e48fd580c98cb7e
Devmal wijesingha <devmalwije1@gmail.com>
2023-03-26 23:08:21 +0530

    optimised model implementations
    Avoid unnecessary operations:
    
    The function is reading and writing to a file on every call, which can cause performance overhead. Consider using a database or caching mechanism to reduce I/O operations.
    The function is creating a new Pandas DataFrame on every call, which can be memory-intensive. Instead, you can create a single DataFrame outside the function and pass it as an argument.
    Use multiprocessing:
    
    If your system has multiple CPUs, you can take advantage of them by using the multiprocessing module to parallelize the function across multiple processes.
    This can speed up the processing time by utilizing the full capacity of your CPU.
    Use numpy and OpenCV functions:
    
    Instead of flattening the pose and face landmarks using a for loop, you can use numpy to reshape the arrays more efficiently.
    You can also use OpenCV's cvtColor function to convert the image to RGB and back to BGR instead of using numpy.

--- fitzen_backend/optimisedModelImp.py
+++ fitzen_backend/optimisedModelImp.py
@@ -17,82 +17,138 @@ mp_holistic = mp.solutions.holistic
 with open("randomForest_pose_classifierV1.pkl", "rb") as f:
     model = pickle.load(f)
 
-# initiating holistic model        
-holistic = mp.solutions.holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5)
+# initiating holistic model
+holistic = mp.solutions.holistic.Holistic(
+    min_detection_confidence=0.5, min_tracking_confidence=0.5)
 
-# create video capture object
-cap = cv2.VideoCapture(0)
-    
+# creating a global DataFrame for pose detection
+X = pd.DataFrame()
+
+# creating a scaler object for standardization
+warnings.filterwarnings('ignore')
+scaler = StandardScaler()
+scaler.fit([[0, 1], [1, 0]])
+
+
+# def image_frame_model(frame):
+#     #getting current timestamp from local system
+#     currentTime = datetime.datetime.now()
+
+#     posture_list = []
+#     posture_dict = {}
+
+#     # Recolor Feed
+#     image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+#     image.flags.writeable = False
+
+#     # Make Detections
+#     results = holistic.process(image)
+
+#     # Recolor image back to BGR for rendering
+#     image.flags.writeable = True
+#     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+#     # Export coordinates
+#     try:
+#         # Extract Pose landmarks
+#         pose = results.pose_landmarks.landmark
+#         pose_row = list(np.array(
+#             [[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in pose]).flatten())
+
+#         # Extract Face landmarks
+#         face = results.face_landmarks.landmark
+#         face_row = list(np.array(
+#             [[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in face]).flatten())
+
+#         # Concate rows
+#         row = pose_row+face_row
+
+#         # Make Detections
+#         X = pd.DataFrame([row])
+#         pose_language_class = model.predict(X)[0]
+#         pose_language_prob = model.predict_proba(X)[0]
+
+#         # filtering out warning from stander scaler due to shaping issues
+#         warnings.filterwarnings('ignore')
+#         X = [[0, 1], [1, 0]]
+#         scaler = StandardScaler()
+#         scaler.fit(X)
+#         posture_list.append(pose_language_class)
+
+#         #writing posture class and detected time to text file
+#         with open("posture.txt", "a") as f:
+#             f.write(f"{pose_language_class},{currentTime}\n")
+
+#     except:
+#         print("No posture detected")
+#         pass
+
+#     # counting the number of proper posture
+#     proper_posture_count = posture_list.count("proper_posture")
+
+#     # calculating the percentage of proper posture
+#     proper_posture_percentage = round(
+#         (proper_posture_count/len(posture_list))*100, 1)
+
+
+#     #adding the posture class and image frame to a dictionary
+#     posture_dict = {'posture_class': pose_language_class, 'image_frame': image}
+
+#     return posture_dict
+
+
 def image_frame_model(frame):
-    #getting current timestamp from local system
-    currentTime = datetime.datetime.now()
-    
+    # getting current timestamp from local system
+    currentTime = datetime.now()
+
     posture_list = []
     posture_dict = {}
 
     # Recolor Feed
     image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    image.flags.writeable = False
 
     # Make Detections
     results = holistic.process(image)
-    # print(results.face_landmarks)
-
-    # Recolor image back to BGR for rendering
-    image.flags.writeable = True
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
     # Export coordinates
     try:
         # Extract Pose landmarks
-        pose = results.pose_landmarks.landmark
-        pose_row = list(np.array(
-            [[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in pose]).flatten())
+        pose = np.array([[landmark.x, landmark.y, landmark.z, landmark.visibility]
+                        for landmark in results.pose_landmarks.landmark]).flatten()
 
         # Extract Face landmarks
-        face = results.face_landmarks.landmark
-        face_row = list(np.array(
-            [[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in face]).flatten())
+        face = np.array([[landmark.x, landmark.y, landmark.z, landmark.visibility]
+                        for landmark in results.face_landmarks.landmark]).flatten()
 
         # Concate rows
-        row = pose_row+face_row
+        row = np.concatenate([pose, face])
+
+        # Standardize the data
+        X = scaler.transform([row])
 
         # Make Detections
-        X = pd.DataFrame([row])
         pose_language_class = model.predict(X)[0]
         pose_language_prob = model.predict_proba(X)[0]
 
-        # filtering out warning from stander scaler due to shaping issues
-        warnings.filterwarnings('ignore')
-        X = [[0, 1], [1, 0]]
-        scaler = StandardScaler()
-        scaler.fit(X)
         posture_list.append(pose_language_class)
-        
-        #writing posture class and detected time to text file
+
+        # writing posture class and detected time to text file
         with open("posture.txt", "a") as f:
             f.write(f"{pose_language_class},{currentTime}\n")
-
-        # if pose_language_class == "proper_posture":
-        #     print("Proper posture")
 
     except:
         print("No posture detected")
         pass
-    
+
     # counting the number of proper posture
     proper_posture_count = posture_list.count("proper_posture")
 
     # calculating the percentage of proper posture
     proper_posture_percentage = round(
         (proper_posture_count/len(posture_list))*100, 1)
-    
-    # #
-    # print("Proper posture percentage: ", proper_posture_percentage)
-    
-    #adding the posture class and image frame to a dictionary
-    posture_dict = {'posture_class': pose_language_class, 'image_frame': image}
-    
+
+    # adding the posture class and image frame to a dictionary
+    posture_dict = {'posture_class': pose_language_class,
+                    'image_frame': cv2.cvtColor(image, cv2.COLOR_RGB2BGR)}
+
     return posture_dict
-
-
