97005a5901b8629b3c96931c9eb307820dd4eaf6
Dinuka Weerasooriya <dinukasanthush50@gmail.com>
2023-04-01 23:05:01 +0530

    Splash Screen
    created a splash screen and auto redirect it to login page. changed the wrapper.dart file

--- fitzen_frontend/lib/wrapper.dart
+++ fitzen_frontend/lib/wrapper.dart
@@ -1,23 +1,24 @@
 import 'package:fitzen_frontend/controllers/user_controller.dart';
 import 'package:fitzen_frontend/views/home.dart';
 import 'package:fitzen_frontend/views/login.dart';
+import 'package:fitzen_frontend/views/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     var userController = Provider.of<UserController>(context);
 
     return FutureBuilder<bool>(
       future: userController.isUserSignedIn(),
-      builder: (context, snapshot){
-        if (snapshot.connectionState == ConnectionState.waiting || snapshot.data == null){
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting ||
+            snapshot.data == null) {
           return Center(child: CircularProgressIndicator());
         }
 
-        return snapshot.data! ? Home() : Login();
+        return snapshot.data! ? SplashScreen() : Login();
       },
     );
   }
