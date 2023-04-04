8f981a6c41dac3845531c238aa0dc86f7811e903
dnadawa <dulajnadawa@gmail.com>
2023-04-04 20:16:01 +0530

    Change route

--- fitzen_frontend/lib/constants.dart
+++ fitzen_frontend/lib/constants.dart
@@ -15,5 +15,5 @@ const POOR_POSTURE_KEY = "poorPostureNotification";
 const LOW_BLINK_COUNT_KEY = "lowBlinkNotification";
 const TWENTY_TWENTY_TWENTY_KEY = "lowBlinkNotification";
 
-const API = "http://localhost:3000/api";
-// const API = "https://fitzen.tech";
+// const API = "http://localhost:3000/api";
+const API = "https://fitzen.tech/api";
