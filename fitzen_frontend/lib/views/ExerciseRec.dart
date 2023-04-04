2080975ce431bd9360bfee9d4b24df13f3a8f480
M N Perera <92733517+MNPerera@users.noreply.github.com>
2023-04-04 18:13:04 +0530

    Update ExerciseRec.dart

--- fitzen_frontend/lib/views/ExerciseRec.dart
+++ fitzen_frontend/lib/views/ExerciseRec.dart
@@ -27,7 +27,8 @@ class VideoPlayerPage extends StatefulWidget {
 class _VideoPlayerPageState extends State<VideoPlayerPage> {
   late VideoPlayerController controller;
   bool isPlaying = true;
-
+  
+  // Routine 1 video list
   final List<String> videoLinks1 = [
     'intro.mp4',
     'R1_ex1.mp4',
@@ -35,6 +36,7 @@ class _VideoPlayerPageState extends State<VideoPlayerPage> {
     'R1_ex3.mp4',
     'R1_ex4.mp4',
   ];
+  // Routine 1 Exercise names list
   final List<String> videoNames1 = [
     'introduction',
     'Exercise 1: Over-And-Backs (10-15 slow reps)',
@@ -42,12 +44,14 @@ class _VideoPlayerPageState extends State<VideoPlayerPage> {
     'Exercise 3: Stand and Reach (5-10 reaches each side, pause at the end position)',
     'Exercise 4: Wall slides with chin nod (2 sets of 10-15 reps)',
   ];
+  // Routine 2 video list
   final List<String> videoLinks2 = [
     'R2_ex1.mp4',
     'R2_ex2.mp4',
     'R2_ex3.mp4',
     'R2_ex4.mp4',
   ];
+  // Routine 2 Exercise names list
   final List<String> videoNames2 = [
     'Exercise 1: Quadruped Thoracic Rotations (10 reps each side with pause at top)',
     'Exercise 2: Kneeling Hip Flexor Stretch (30-45 seconds holds each side)',
@@ -58,7 +62,7 @@ class _VideoPlayerPageState extends State<VideoPlayerPage> {
   @override
   void initState() {
     super.initState();
-    controller = VideoPlayerController.asset('assets/videos/${videoLinks1[0]}')
+    controller = VideoPlayerController.asset('assets/videos/${videoLinks1[0]}')   // accessing the videos from the video lists
       ..initialize().then((_) {
         setState(() {});
       });
@@ -81,7 +85,7 @@ class _VideoPlayerPageState extends State<VideoPlayerPage> {
           child: Center(
             child: AspectRatio(
               aspectRatio: 16 / 9,
-              child: Stack(
+              child: Stack(  // adding the playpause button
                 alignment: Alignment.bottomCenter,
                 children: [
                   VideoPlayer(controller),
@@ -106,20 +110,17 @@ class _VideoPlayerPageState extends State<VideoPlayerPage> {
             ),
           ),
         ),
-        const SizedBox(height: 20),
+        const SizedBox(height: 20),   // adding a small gap between the 2 routines with a empty SizedBox
 
         // ----------------------------------
-        // Column(
-        // adding the routine 1 text above second link list by wrapping the ListView.builder with column and adding text above it.
-        // children: [
-        const Text(
+        const Text(   // first routine heading
           'Routine 1',
           style: TextStyle(
             fontSize: 24,
             fontWeight: FontWeight.bold,
           ),
         ),
-        Expanded(
+        Expanded(  // looping through the link list 1 and accessing the videos after clicked
           child: ListView.builder(
             itemCount: videoLinks1.length,
             itemBuilder: (BuildContext context, int index) {
@@ -151,18 +152,15 @@ class _VideoPlayerPageState extends State<VideoPlayerPage> {
         //   ],
         // ),
         const SizedBox(height: 20),
-        // ----------------------------------------
-        // Column(
-        // adding the routine 1 text above second link list by wrapping the ListView.builder with column and adding text above it.
-        // children: [
-        const Text(
+        // ---------------------------------------
+        const Text(  // second routine heading
           'Routine 2',
           style: TextStyle(
             fontSize: 24,
             fontWeight: FontWeight.bold,
           ),
         ),
-        Expanded(
+        Expanded(  // looping through the link list 2 and accessing the videos after clicked
           child: ListView.builder(
             itemCount: videoLinks2.length,
             itemBuilder: (BuildContext context, int index) {
