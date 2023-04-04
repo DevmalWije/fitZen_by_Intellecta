aa89ec39337e29210269c4c3c891070e5bf66ec7
Devmal wijesingha <devmalwije1@gmail.com>
2023-04-04 18:42:05 +0530

    added scoring capability for assigning users scores based on their past and current sessions

--- fitzen_backend/multiThreadingTest.py
+++ fitzen_backend/multiThreadingTest.py
@@ -1,11 +1,14 @@
 import cv2
 import asyncio
 import multiThreadingVersion
-import postureScoring
+import calculations
 goodCount = 0
+badCount = 0
+
 
 async def process_frame(frame):
     global goodCount
+    global badCount
     dict = await multiThreadingVersion.image_frame_model(frame)
     print(dict['good_posture_percent'])
     cv2.imshow('g', dict['image_frame'])
@@ -19,7 +22,7 @@ while cap.isOpened():
     asyncio.run(process_frame(frame))
 
     if cv2.waitKey(10) & 0xFF == ord('q'):
-        print(goodCount)
+        calculations.calculate_posture_score(goodCount, badCount, 0)
         # posture_score = postureScoring.calculate_posture_score(int(dict['good_posture_count']), int(dict['bad_posture_count']), 1)
         break
 
